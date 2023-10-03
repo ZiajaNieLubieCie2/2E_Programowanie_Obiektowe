01719bce3cd95bc5a7c955ad7c87098061f64141
ZiajaNieLubieCie2 <tutaj.jakub1@gmail.com>
2023-10-03 14:57:02 +0200

    zadanie pierwsze zrobione

---
+++ 03.10.2023/zadanie 1.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l;
+		cout << "podaj liczbe marki od 1 do 30" << endl;
+	cin >> l;
+	if (l >= 30)
+	{
+		cout << "bleda liczba " << endl;
+	}
+
+
+	switch (l)
+	{
+	case 1:
+		cout << " maraka to bmw " << endl; break;
+	case 2:
+		cout << " maraka to seat " << endl; break;
+	case 3:
+		cout << " maraka to fiat" << endl; break;
+	case 4:
+		cout << " maraka to audi" << endl; break;
+	case 5:
+		cout << " maraka to skoda" << endl; break;
+	case 6:
+		cout << " maraka to toyota" << endl; break;
+	case 7:
+		cout << " maraka to dacia" << endl; break;
+	case 8:
+		cout << " maraka to ford" << endl; break;
+	case 9:
+		cout << " maraka to renault " << endl; break;
+	case 10:
+		cout << " maraka to mercedes" << endl; break;
+	case 12:
+		cout << " maraka to nissan" << endl; break;
+	case 13:
+		cout << " maraka to alpina" << endl; break;
+	case 14:
+		cout << " maraka to land rover" << endl; break;
+	case 15:
+		cout << " maraka to lexus" << endl; break;
+	case 16:
+		cout << " maraka to mazda " << endl; break;
+	case 17:
+		cout << " maraka to mclaren" << endl; break;
+	case 18:
+		cout << " maraka to maserati " << endl; break;
+	case 19:
+		cout << " maraka to dodge " << endl; break;
+	case 20:
+		cout << " maraka to mini" << endl; break;
+	case 21:
+		cout << " maraka to porshe " << endl; break;
+	case 22:
+		cout << " maraka to rolls royce" << endl; break;
+	case 23:
+		cout << " maraka to smart " << endl; break;
+	case 24:
+		cout << " maraka to suzuki" << endl; break;
+	case 25:
+		cout << " maraka to subaru " << endl; break;
+	case 26:
+		cout << " maraka to volvo" << endl; break;
+	case 27:
+		cout << " maraka to kia " << endl; break;
+	case 28:
+		cout << " maraka to opel " << endl; break;
+	case 29:
+		cout << " maraka to man " << endl; break;
+	case 30:
+		cout << " maraka to jeep " << endl; break;
+
+	}
+	
+
+	return 0;
+
+
+}
