89ae97ffaee824d4d4a63a69e93cea8a18d1c216
ZiajaNieLubieCie2 <tutaj.jakub1@gmail.com>
2023-10-10 14:20:58 +0200

    zadanie pierwsze i drugie zrobione

---
+++ 10.10.2023/zadanie drugie.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string login, haslo;
+	do {
+		cout << "podaj login: " << endl;
+		cin >> login;
+		cout << "podaj haslo: " << endl;
+		cin >> haslo;
+		if (login == "admin" && haslo == "haslo") {
+
+			cout << "gratulacje zostales zalogowany " << endl;
+			break;
+		}
+		
+		else if (login == "admin1" && haslo == "haslo1") {
+
+			cout << "gratulacje zostales zalogowany " << endl;
+			break;
+		}
+		else if (login == "admin2" && haslo == "haslo2") {
+
+			cout << "gratulacje zostales zalogowany " << endl;
+			break;
+		}
+		else
+		
+			cout << "niestety nie zostales zalogowany " << endl;
+		
+	} while (true);
+
+	return 0;
+}
---
+++ 10.10.2023/zadanie pierwsze.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string login, haslo;
+	cout << "podaj login: " << endl;
+	cin >> login;
+	cout << "podaj haslo: " << endl;
+	cin >> haslo;
+	if (login == "admin" && haslo == "haslo")
+	{
+		cout << "gratulacje zostales zazlogowany (;" << endl;
+	}
+	else
+	{
+		cout << "niestety nie zostales zalogowany " << endl;
+	}
+	return 0;
+}
