134d0f966a6a459fc871c510847a4c2d53ed91bd
ZiajaNieLubieCie2 <tutaj.jakub1@gmail.com>
2023-10-17 15:01:25 +0200

    zadanie pierwsze i druie zrobione

---
+++ 17.11.23/haslo_I_brakdostepu.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "podaj pierwsza liczbe parzysta: " << endl;
+	cin >> x;
+	if (x != 0) {
+		cout << "podaj druga liczbe parzysta: " << endl;
+	}
+}
---
+++ 17.11.23/zadaniedrugie.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int z;
+    cout << "Podaj pierwsza liczbe parzysta" << endl;
+    cin >> x;
+    do {
+        if (x % 2 == 0) {
+
+        }
+        else {
+            cout << "Podales liczbe nieparzysta" << endl;
+            cout << "Podaj pierwsza liczbe parzysta jeszcze raz" << endl;
+            cin >> x;
+        }
+        cout << "Podaj druga liczbe parzysta" << endl;
+        cin >> y;
+        if (y % 2 == 0) {
+
+        }
+        else {
+            cout << "Podales liczbe nieparzysta" << endl;
+            cout << "Podaj jeszcze raz druga liczbe parzysta" << endl;
+            cin >> y;
+        }
+        if (x % 2 == 0 && y % 2 == 0)
+        {
+            z = y * x;
+            cout << "Wynik to: " << z << endl;
+            break;
+        }
+    } while (false);
+
+    return 0;
+}
