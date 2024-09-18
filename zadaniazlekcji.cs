c5787feb78d0ca87e05789d8837e28414df12bcc
ZiajaNieLubieCie2 <145669243+ZiajaNieLubieCie2@users.noreply.github.com>
2024-09-18 13:08:41 +0200

    18.09.2024

---
+++ zadaniazlekcji.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Program
+{
+    private static string new_String;
+
+    public static void Main(string[] args)
+
+    {
+        double i = 3.14;
+        Console.WriteLine(i);
+
+        string j = "Jakub Tutaj";
+        Console.WriteLine(j);
+
+        int x = 10;
+        double y = (Convert.ToDouble(x));
+        Console.WriteLine(y);
+
+
+        double a = 10.78493;
+        int b = Convert.ToInt32(a);
+        Console.WriteLine(b);
+
+        Console.WriteLine("podaj napis");
+        string napis = Console.ReadLine();
+        Console.WriteLine(napis);
+    }
+}
